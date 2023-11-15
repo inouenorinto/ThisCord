ea6a75fe2996a42125362dd181711551b2cbbfc8
suzuki <ys22304011@ga.ttc.ac.jp>
2023-11-15 16:04:59 +0900

    MessageDataDAO.insertRecordを追加(追加中)

--- ThisCode/src/main/java/commands/TestCommand.java
+++ ThisCode/src/main/java/commands/TestCommand.java
@@ -1,27 +1,11 @@
 package commands;
 
-import java.util.ArrayList;
-
-import bean.ServerDataBean;
-import db.dao.ServerDataDAO;
+import db.dao.MessageDataDAO;
 
 public class TestCommand {
 	public static void main(String[] args) {
-		ArrayList<ServerDataBean> serverdatas = null;
-		ServerDataBean serverData = new ServerDataBean();
-		ServerDataDAO serverdatadao = ServerDataDAO.getInstance();
-		//UserDataDAO userdatadao = UserDataDAO.getInstance();
-		//UserServerRelationshipDAO serverRelationdao =
-				//UserServerRelationshipDAO.getInstance();
-		
-		serverdatas = serverdatadao.findAll();
-		
-		for (ServerDataBean bean : serverdatas) {
-			System.out.print("serverid: " + bean.getServer_id() + "\t");
-			System.out.print("name: " + bean.getServer_name() + "\t");
-			System.out.print("hostid: " + bean.getHost_id() + "\t");
-			System.out.println("icon" + bean.getServer_icon());
-		}
+		MessageDataDAO mddao = MessageDataDAO.getInstance();
+		mddao.insertRecord(1, 1, "'2023/11/15'", "'aaaaa'");
 
 	}
 }
--- ThisCode/src/main/java/db/dao/MessageDataDAO.java
+++ ThisCode/src/main/java/db/dao/MessageDataDAO.java
@@ -14,12 +14,24 @@ public class MessageDataDAO{
     		+ "WHERE server_id = ? && channel_id = ? ";
     private static final String UPDATE_MESSAGE = "UPDATE message_data SET message = ? "
             + "WHERE server_id = ? AND channel_id = ?";
+    private static final String INSERT_MESSAGE = "INSERT INTO message (user_id, channel_id, send_date, message) "
+    		+ "VALUES(?, ?, ?, ?)";
 
     Connection cn;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
-
-    public MessageDataDAO(Connection cn){
+    
+    private static MessageDataDAO mddao = null;
+    
+    static {
+    	mddao = new MessageDataDAO();
+    }
+    
+    public static final MessageDataDAO getInstance() {
+    	return mddao;
+    }
+    
+    private MessageDataDAO(){
         this.cn = MySqlManager.getConnection();
     }
 
@@ -37,7 +49,7 @@ public class MessageDataDAO{
                 messageBean.setUser_id(rs.getString("user_id"));
                 messageBean.setSend_date(rs.getString("send_date"));
                 messageBean.setMessage(rs.getString("message"));
-                result.add(messageBean); 
+                result.add(messageBean);
             }
 
         } catch (SQLException e){
@@ -87,5 +99,28 @@ public class MessageDataDAO{
             }
         }
         return success;
+    }
+    public void insertRecord(int user_id, int channel_id, String send_date, String message) {
+    	try {
+	    	pstmt = cn.prepareStatement(INSERT_MESSAGE);
+	    	pstmt.setInt(1, user_id);
+	    	pstmt.setInt(2, channel_id);
+	    	pstmt.setString(3, send_date);
+	    	pstmt.setString(4, message);
+	    	
+	    	pstmt.executeUpdate();
+	    	
+	    	cn.commit();
+    	} catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
