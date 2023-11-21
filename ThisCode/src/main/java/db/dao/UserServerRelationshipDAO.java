60d769abf0979f3d9e0966835b6c50325e667752
okada <to22304004@ga.ttc.ac.jp>
2023-11-21 14:24:44 +0900

    メッセージを1行だけ持ってくるメソッドを追加

--- ThisCode/src/main/java/db/dao/UserServerRelationshipDAO.java
+++ ThisCode/src/main/java/db/dao/UserServerRelationshipDAO.java
@@ -11,7 +11,9 @@ import bean.UserServerRelationshipBean;
 import util.mysql.MySqlManager;
 
 public class UserServerRelationshipDAO {
-	private static final String SERVER_SELECT = "select * from user_server_relationship where user_id = ? and server_id = ?"; //これでいいかわからん
+	private static final String SERVER_SELECT = "select * from user_server_relationship"; //これでいいかわからん
+	private static final String FINDRECORD = "select * from user_server_relationship where user_id = ? and server_id = ?";
+
 	
 	Connection cn = null;
 	PreparedStatement pstmt = null;
@@ -65,6 +67,44 @@ public class UserServerRelationshipDAO {
         }
         return result;
     }
+	
+	
+	
+	public UserServerRelationshipBean findRecord(int id) {
+        UserServerRelationshipBean userServerRelationshipBean = new UserServerRelationshipBean();
+        
+        try{
+            pstmt = cn.prepareStatement(FINDRECORD);
+            pstmt.setInt(1, id);
+            rs = pstmt.executeQuery();
+
+            while(rs.next()) {
+                userServerRelationshipBean.setUser_id(rs.getInt("user_id"));
+                userServerRelationshipBean.setServer_id(rs.getInt("server_id"));
+
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        } finally {
+            if(pstmt != null) {
+                try{
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (cn != null) {
+                try{
+                    cn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return userServerRelationshipBean;
+    }
+
+	
 	
 	public Integer[] getServers(int user_id) {
 		ArrayList<Integer> result = new ArrayList<>();
