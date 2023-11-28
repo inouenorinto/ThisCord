41a0dc642971b1fbce5ec51f26d9acd684cf947d
Kanz <48710520+cableguy123@users.noreply.github.com>
2023-11-28 09:37:34 +0900

    競合

--- ThisCode/src/main/java/util/mysql/MySqlManager.java
+++ ThisCode/src/main/java/util/mysql/MySqlManager.java
@@ -2,27 +2,33 @@ package util.mysql;
 
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class MySqlManager {
     public static Connection getConnection() {
-        String DATABASE_NAME = "thiscord";
-        String PROPATIES = "?characterEncoding=UTF-8&useTimezone=true&serverTimezone=Asia/Tokyo";
-        String URL = "jdbc:mySQL://localhost/" + DATABASE_NAME+PROPATIES;
-
-        String USER = "testuser1";
-        String PASS = "password";
-        Connection conn = null;
-        try {
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            conn = DriverManager.getConnection(URL, USER, PASS);
-            
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return conn;
+//        String DATABASE_NAME = "thiscord";
+//        String PROPATIES = "?characterEncoding=UTF-8&useTimezone=true&serverTimezone=Asia/Tokyo";
+//        String URL = "jdbc:mySQL://localhost/" + DATABASE_NAME+PROPATIES;
+//    	Connection cn = null;
+//    	PrintWriter out = null;
+//    	
+//    	try {
+//            String uri = "jdbc:mysql://mydatabase.ccsxuhcaf1js.ap-northeast-1.rds.amazonaws.com:3306/thiscord";
+//            Class.forName("com.mysql.jdbc.Driver");
+//            cn = DriverManager.getConnection(uri,"testuser1","password");
+//            System.out.println("Connection Success!");
+//            PreparedStatement ps = (PreparedStatement)cn.prepareStatement("SELECT * FROM user_data");
+//            ResultSet rs = ps.executeQuery();
+//            System.out.println("SELECT!");
+//            while(rs.next()) {
+//                   String number = rs.getString("mailaddress");
+//                   out.print(number);
+//            }
+//          }catch(Exception e) {
+//        	  		System.out.println("Connection fail!");
+//                    out.println(e.toString());
+//            }
+    	Connection cn = null;
+    	
+        return cn;
     }
 }
