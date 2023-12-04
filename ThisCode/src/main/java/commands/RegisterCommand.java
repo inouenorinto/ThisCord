f5554e4532924cd6628cea6f1f59ac086ee04731
suzuki <ys22304011@ga.ttc.ac.jp>
2023-12-04 10:16:35 +0900

    ハッシュ化有効

--- ThisCode/src/main/java/commands/RegisterCommand.java
+++ ThisCode/src/main/java/commands/RegisterCommand.java
@@ -20,10 +20,12 @@ public class RegisterCommand extends AbstractCommand {
         String password = req.getParameter("password")[0];
         String email = req.getParameter("email")[0];
         
+        String hashedPassword = Encryption.hash(password);	//パスワードのハッシュ化
+        
         RegisterUserDTO dto = new RegisterUserDTO();
         dto.setEmail(email);
         dto.setUser_name(username);
-        dto.setPassword(password);
+        dto.setPassword(hashedPassword);
         dto.setUser_icon(editedImageData);
         if(dto.getUser_icon().substring(1) == null) {
         	System.out.println("null");
